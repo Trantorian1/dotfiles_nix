c1ba84e5578f140d4a8c58ed50e803f9bd490a55
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-08-17 15:01:48 +0200

    feat(nix): librewolf as default browser

--- configuration.nix
+++ configuration.nix
@@ -119,6 +119,10 @@ in {
     # Set fish as the default shell
     programs.fish = {
       enable = true;
+      shellAliases = {
+        nvim = "nix-shell /home/trantorian/Documents/code/dotfiles/nvim --arg path $(pwd) --command 'neovide --frame none -- -u $CONFIG/init.lua && exit'";
+        e = "exit";
+      };
       interactiveShellInit = ''
         # Disable the default greeting
         set fish_greeting
@@ -152,10 +156,49 @@ in {
         set fish_pager_color_description 9ca0b0
       '';
     };
-    home.shell.enableFishIntegration = true;
 
-    # Install firefox.
-    programs.firefox.enable = true;
+    # Install Librewolf
+    programs.librewolf = {
+      enable = true;
+      settings = {
+        # Enabling google safe browsing
+        # https://librewolf.net/docs/settings/#enable-google-safe-browsing
+        "browser.safebrowsing.malware.enabled" = true;
+        "browser.safebrowsing.phishing.enabled" = true;
+        "browser.safebrowsing.blockedURIs.enabled" = true;
+        "browser.safebrowsing.provider.google4.gethashURL" = "https://safebrowsing.googleapis.com/v4/fullHashes:find?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST";
+        "browser.safebrowsing.provider.google4.updateURL" = "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST";
+        "browser.safebrowsing.provider.google.gethashURL" = "https://safebrowsing.google.com/safebrowsing/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2";
+        "browser.safebrowsing.provider.google.updateURL" = "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2&key=%GOOGLE_SAFEBROWSING_API_KEY%";
+        "browser.safebrowsing.downloads.enabled" = true;
+
+        # Do not restore tabs on crash
+        # https://librewolf.net/docs/settings/#stop-librewolf-from-resuming-after-a-crash
+        "browser.sessionstore.resume_from_crash" = false;
+
+        # Stricter autoplay prevention
+        # https://librewolf.net/docs/settings/#use-a-stricter-autoplay-policy
+        "media.autoplay.blocking_policy" = 2;
+
+        # Disable history
+        "places.history.enabled" = false;
+
+        # Homepage
+        "browser.startup.homepage" = "https://discord.com/login|https://app.gather.town/signin|https://slack.com/signin#/signin|https://app.deel.com/login|https://app.rippling.com/sign-in/id|https://id.atlassian.com/login|https://claude.ai/login|https://account.proton.me/mail|https://account.proton.me/calendar|https://github.com/login|https://habitica.com/login|https://www.odoo.com/web/login|https://web.whatsapp.com/|https://web.telegram.org/|https://tidal.com/login/";
+
+        # Use vertical tabs
+        "sidebar.verticalTabs" = true;
+
+        # Default extensions
+        "browser.policies.runOncePerModification.extensionsInstall" = ''
+          [
+            "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi"
+            "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi"
+            "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi"
+          ]
+        '';
+      };
+    };
 
     # Setting up git
     programs.git = {
@@ -165,6 +208,7 @@ in {
       extraConfig = {
         core = {
           pager = "";
+          editor = "nvim";
         };
       };
     };
