55f12ed1777609dee994f69e7c347bc49f943c29
trantorian <114066155+Trantorian1@users.noreply.github.com>
2025-08-31 20:36:14 +0200

    feat(plugin): indent

--- nvim/lua/custom/plugins/snacks.lua
+++ nvim/lua/custom/plugins/snacks.lua
@@ -1,6 +1,13 @@
 return {
 	"folke/snacks.nvim",
 	---@type snacks.Config
-	opts = {},
+	opts = {
+		indent = {
+			enabled = true,
+			animate = {
+				enabled = false,
+			},
+		},
+	},
 }
 -- vim: ts=2 sts=2 sw=2 et
